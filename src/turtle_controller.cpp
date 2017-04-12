c830ad832c7141ddc254e06cebf54990f0443e6c
Jack & Mat <mateusz.wagner@student.put.poznan.pl>
2017-04-12 16:36:46 +0200

    Adds turtle controller.

---
+++ src/turtle_controller.cpp
@@ -0,0 +1,26 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "turtle_controller");
+  ros::NodeHandle nh;
+
+  ros::Publisher control_pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
+
+  ros::Rate rate(100.0);
+
+  while (ros::ok()) {
+    geometry_msgs::Twist controls;
+
+    controls.linear.x = 1.0; // [1 m/s]
+    controls.angular.z = 1.0; // [1 rad/s]
+
+    control_pub.publish(controls);
+
+    rate.sleep();
+  }
+
+  return 0;
+}
