755a7a1eeb393d148c22ca986bc658cdaa160b13
Jack & Mat <mateusz.wagner@student.put.poznan.pl>
2017-05-17 15:13:59 +0200

    labs

--- src/pushup_maker.cpp
+++ src/pushup_maker.cpp
@@ -1,8 +1,31 @@
 #include <ros/ros.h>
 #include <iostream>
-#include "pushup_maker.h"
 
-int main(int argc, char **argv)
+bool I_am_tired = 0;
+
+ros::Timer timer;
+
+void timercallback(const ros::TimerEvent&);
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "Pushup");
+  ros::NodeHandle nh;
+
+  timer = nh.createTimer(ros::Duration(1), timercallback);
+
+  ros::spin();
+
+  return 0;
+}
+
+void timercallback(const ros::TimerEvent&)
 {
-    return 0;
+  static int counter = 0;
+  counter = counter+1;
+  ROS_INFO_STREAM("I did "<<counter<<" pushup !");
+  if(counter>=10)
+  {
+    ROS_INFO_STREAM("I am so tired...");
+    timer.stop();
+  }
 }
